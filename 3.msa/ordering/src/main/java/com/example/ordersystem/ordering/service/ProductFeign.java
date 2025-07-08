8f98a8f95d6dafabff4b6422058004a2d50de418
yeonnex <yeonnex@gmail.com>
2025-07-08 14:43:24 +0900

    feat: order, product 서비스 배포 추가

--- 3.msa/ordering/src/main/java/com/example/ordersystem/ordering/service/ProductFeign.java
+++ 3.msa/ordering/src/main/java/com/example/ordersystem/ordering/service/ProductFeign.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 //name은 eureka에 등록된 호출할 서비스의 이름
 //url은 k8s에서 service명
-@FeignClient(name = "product-service")
+@FeignClient(name = "product-service", url="http://product-service")
 public interface ProductFeign {
 
     @GetMapping("/product/{productId}")
